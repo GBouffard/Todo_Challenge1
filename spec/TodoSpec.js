f1c243688cb28d75730db74bcb8fc71a4f061858
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-05-03 17:17:54 +0100

    ready for testing

--- spec/TodoSpec.js
+++ spec/TodoSpec.js
@@ -3,4 +3,34 @@ describe('Todo', function(){
   beforeEach(function(){
     todo = new Todo();
   });
+
+  xit('has an empty list of tasks when created', function(){
+  });
+
+  xit('can add tasks to its list', function(){
+  });
+
+  xit('knows that a new added task\'s state is not done (false) when created', function(){
+  });
+
+  xit('can replace the status of a task to done (true) when done', function(){
+  });
+
+  xit('cannot do a task if it\'s already been done', function(){
+  });
+
+  xit('can show all tasks and its number', function(){
+  });
+
+  xit('can show all active tasks and its number', function(){
+  });
+
+  xit('can show all completed tasks and its number', function(){
+  });
+
+  xit('can clear the full list of tasks', function(){
+  });
+
+  xit('can deleted all completed tasks', function(){
+  });
 });
