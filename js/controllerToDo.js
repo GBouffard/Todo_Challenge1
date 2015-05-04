60b062710579c89ceb3c98062cf7655529b97afc
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-05-04 22:12:32 +0100

    Mocking the behaviour of entering a new task

--- js/controllerToDo.js
+++ js/controllerToDo.js
@@ -1,3 +1,7 @@
 todo1.controller('ControllerToDo', [function() {
   var self = this;
+    
+  self.tasksList = {
+    "tasks": ["walk the dog","buy some milk"]
+  };
 }]);
--- test/controllerToDo.spec.js
+++ test/controllerToDo.spec.js
@@ -10,4 +10,12 @@ describe('ControllerToDo', function() {
   it('has no task in the in the add a task box when created', function() {
     expect(ctrl.newTask).toBeUndefined();
   });
+
+  describe('when entering a new task', function() {
+    var tasks = ["walk the dog","buy some milk"];
+
+    it('displays the new tasks', function() {
+      expect(ctrl.tasksList.tasks).toEqual(tasks);
+    });
+  });
 });
